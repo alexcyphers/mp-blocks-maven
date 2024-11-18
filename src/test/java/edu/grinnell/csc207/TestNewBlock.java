8b168509a1eb3a7e88fb1b98d4eb27df81393f84
cyphersa <cyphersa@grinnell.edu>
2024-11-17 21:36:40 -0600

    Trying to get testing

--- src/test/java/edu/grinnell/csc207/TestNewBlock.java
+++ src/test/java/edu/grinnell/csc207/TestNewBlock.java
@@ -2,11 +2,19 @@ package edu.grinnell.csc207;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import edu.grinnell.csc207.blocks.AsciiBlock;
 import edu.grinnell.csc207.blocks.Boxed;
 import edu.grinnell.csc207.blocks.Empty;
+import edu.grinnell.csc207.blocks.HAlignment;
+import edu.grinnell.csc207.blocks.HComp;
+import edu.grinnell.csc207.blocks.Line;
+import edu.grinnell.csc207.blocks.Rect;
+import edu.grinnell.csc207.blocks.Trimmed;
+import edu.grinnell.csc207.blocks.VAlignment;
+import edu.grinnell.csc207.blocks.VComp;
 
 /**
  * Tests of the new block.
@@ -16,6 +24,7 @@ public class TestNewBlock {
   // | Tests |
   // +-------+
 
+  static AsciiBlock alpha;
   /**
    * A placeholder.
    */
@@ -25,16 +34,56 @@ public class TestNewBlock {
   } // placeholder()
 
 
+    /**
+   * Set up the global.
+   */
+  @BeforeAll
+  public static void setup() throws Exception {
+    try {
+      // Globals for HComp tests
+      alpha = new VComp(HAlignment.LEFT, new AsciiBlock{"abcde"}) {
+        
+      };
+      
+      
+      
+      
+      new Line("abcde"),
+                         new Line("fghij"),
+      new Line("klmno"),
+      new Line("pqrst"),
+      new Line("uvwxy"));
+    } catch (Exception e) {
+      // Do nothing; we shouldn't get exceptions.
+    } // try/catch
+
+
+
   /**
    * Do we successfully build the empty block?
    */
   @Test
   public void testTrimmedEmpty() {
-    AsciiBlock trimmed = new Boxed(new Empty());
+    //AsciiBlock block = new HComp(HAlignment.LEFT, );
+    AsciiBlock[] block = new AsciiBlock[]{new Line("abcde"),
+                                          new Line("fghij"),
+                                          new Line("klmno"),
+                                          new Line("pqrst"),
+                                          new Line("uvwxy")};
+
+    
+    AsciiBlock trimmed = new Trimmed(block, HAlignment.LEFT, 
+                                     VAlignment.TOP, 2,
+                                     2);
+
+    
   }
 
 
-  
+
+
+
+
   
 
 } // class TestNewBlock
